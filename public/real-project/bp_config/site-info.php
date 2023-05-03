ebb0c24cd9058beb7249e09ebbb9620b20361529
hassan <hassan@thiqatech.com>
2023-05-03 19:33:28 +0400

    site content done

--- public/real-project/bp_config/site-info.php
+++ public/real-project/bp_config/site-info.php
@@ -4,5 +4,3 @@
 //********************-->
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'helper.php';
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'verbiage.php';
-
